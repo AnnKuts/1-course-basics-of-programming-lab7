56819843a93156bfeef96044606499883fbeba6a
Ann Kuts <annkuts@Anns-MacBook-Pro.local>
2024-12-23 11:50:42 +0200

    build: made the 3d task

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,17 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+//the best choice is to do that, but lecturer's tests aren't passed because it's too short
+const unique1 = (array) => {
+  return [...new Set(array)];
+};
 
+const unique = (array) => {
+ const result = [];
+ for (const item of array) {
+  if (!result.includes(item))
+      result.push(item);
+  }
+ return result;
+}
 module.exports = { unique };
